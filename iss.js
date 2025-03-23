5fc48262711d9977cdd5ef6ad22fac2b5f9196f5
JaxonC-06 <jaxoncostain@outlook.com>
2025-03-23 16:21:00 -0600

    The iss.js and index.js files are both fully functional.

---
+++ iss.js
@@ -0,0 +1,20 @@
+const needle = require("needle");
+
+const fetchMyIP = function(callback) {
+  needle.get('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    return callback(null, body);
+  });
+};
+
+module.exports = { fetchMyIP };
