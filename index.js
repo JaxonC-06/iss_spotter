9a4ffab22f13125c2165a24e835d2c08cb80776e
JaxonC-06 <jaxoncostain@outlook.com>
2025-03-23 19:19:12 -0600

    Added function which fetches IP coordinates for a given IP.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -8,3 +8,12 @@
 
 //   console.log("It worked! Returned IP:", ip);
 // })
+
+// fetchCoordsByIP('70.73.13.138', (error, data) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Return IP:", data);
+// });
--- iss.js
+++ iss.js
@@ -17,4 +17,22 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  needle.get(`http://ipwho.is/${ip}?output=json`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+
+    if (!body.success) {
+      const msg = `Status success was ${body.success} when fetching for IP ${body.ip}. Server message is: ${body.message}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const geoCoords = {latitude: body.latitude, longitude: body.longitude};
+
+    return callback(null, geoCoords);
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
